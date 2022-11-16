9c39f17f1c7be2716a2afc3c4f87a83b27fc5649
adamstucci <adamstucci@gmail.com>
2022-11-16 13:41:50 +1100

    cleanup

--- set1/bitlib.c
+++ set1/bitlib.c
@@ -13,14 +13,6 @@
 #define TOP_TWO 0xc0
 #define BOT_TWO 0x03
 
-//NEED TO CAST USE UNSIGNED WHERE POSSIBLE
-
-//need to add if 0 is the only digit then allow zeroes
-//could properly do it by counting bits instead of truncate 0...what if instead of numbers just want bit string with padded 0s
-//my hack is wrong anyway..e.g ff will need 0s padded
-//assume takes valid hex symbol
-//prev soltn was to refactor binary so it is well formed...required some calculation and/or passing over thw string i.e round to a multiple of 6...why did I stop this
-//alternatively count bytes and remainder and recognise need for padding
 unsigned char sym2num(char c) {
 	if (c >= 'A' && c <= 'Z') return c - 'A' + 10;
 	if (c >= 'a' && c <= 'z') return c - 'a' + 10;
@@ -39,12 +31,10 @@ char bin2sym64(unsigned char c) {
 
 unsigned char *hex2bin(char *hex, int *bin_len) {
 	int len = strlen(hex);
-	//int new_len = len/2 + 2; //+2 one for extra space, one for null terminator
 	int new_len = len/2 + (len % 2); //exactly how many array elements will be needed
 	unsigned char *new = malloc(sizeof(unsigned char) * new_len); //at most 1 extra wasted byte
 	assert(new != NULL);
 	memset(new, 0, new_len);
-	//the 'leading' 4 can be all zero but not the other group of 4
 
 	int i = len -1; //least sig character in the hex string
 	int j = 0; //least sig position in the binary array
@@ -55,45 +45,18 @@ unsigned char *hex2bin(char *hex, int *bin_len) {
 	while (i >= 0) { //while still processing characters
 		curr = hex[i];
 		curr = sym2num(curr); //takes the symbol and gives a number that can be fit in 4 bits
-		//printf("%d\n", curr);
-		//will bit shifting zero do what I want...just make it a ternary
+		//will bit shifting zero do what I want...just make it a ternary...bit shift by 0 well defined
 		new[j] |= (parity == 0 ? curr : curr << 4);//what do you do with the low and high order...technically it doesn't matter...put the low order bits in the low order slot...when grouping endianess screws things up anyway
 		if (parity == 1) j++; //move to next byte
 		parity = (parity + 1) % 2;
 		i--; //each iteration looking at new word in sequence
 	}
-	// if (parity == 1) j++;
-	// *bin_len = j;
-	//pretty sure bin_len will just be new_len
+
 	*bin_len = new_len;
 	return new; //mem zeroed out...no need to explicitly set null terminator
 }
 
-//still getting an off by 1 error
-//can't use the null terminator in binary need to be aware of length
-//need code to truncate leadding 0
-//len in number of bytes
-// is there an easy way to know how many symbols I'll need...would need to know how it's divisible by 6 and what would be padding
-//padding must be in last array element
-// if perfectly divisible by 6 then padding could only be in the top 6 bits of the last.. as if there was more padding
-// then the whole byt could be removed....if the whole byte is zero then the bottom two bits must be for an actual symbol from before
-// if the top 4 bits from the other one isn't an an actual symbol then a leading zero was inputted to generate the extra one
-// or those top 4 are padding in which case an extra one wasn't needed
-// man these cases are messy
-// need to actually prove they are correct
-
-// if not purely divisible by 6....hard to actually know unless our stray bits that aren't in a perfect 6 are zero or not
-// needs to be determined at runtime
-// only way to create the array ahead of time is to know the bits
-// only want to do this if faster then reverse the array
-// only want to reverse the array so i can easily print with c built ins
-// but want to do most of the stuff in binary anyway
-// should i even have this sort of twostring or just define my own print on the binary data that goes character by character
-// tostring would be useful but if using my own special print anyway may as well just operate on binary
-// want a to string that behaves like the C string
-
 char *bin2six(unsigned char *binary, int len) {
-	//calculating total bits wrong...sizeof is in bytes not bits
 	int total_bits = len * sizeof(char) * 8;
 	int new_len = total_bits/6 + 1; //div by 6 for number of b64 symbols + partial symbol
 	char *new = malloc(sizeof(char) * (new_len + 1));
--- set1/s1c1.c
+++ set1/s1c1.c
@@ -5,16 +5,6 @@
 
 #include "bitlib.h"
 
-// TODO original code sort of working but there is a buffer overflow going on i think
-// can't believe i've wasted so much time today
-// i used to be smarter
-// don't need leading bit or anything...break up into cases
-// if we start a 'token' in the last byte and it's zero then we no it's just padding
-// to properly make the number of characters though in a single allocation at the beginning so can generate the string without
-// reversing or special print need to know at what point would we start a token in the last one
-// and when would it be 0...which is what the leading bit gives us
-// maybe edit this code to add leading bit??
-
 #define BUF_SIZE 1024
 
 int main(int argc, char **argv) {
@@ -24,15 +14,6 @@ int main(int argc, char **argv) {
 	if (buffer[len-1] == '\n') buffer[len-1] = '\0'; //chomp
 	int bin_len = 0;
 	unsigned char *bin = hex2bin(buffer, &bin_len);
-	// printf("%d\n", bin_len);
-	//int len2 = strlen(bin);
-	//printf("bin is %d chars long\n", len2);
-	//printf("%d\n", bin[0]);
-	//print_bin(bin);
-	//printf("\n");
-	// char *six = bin2six(bin, bin_len);
-	// print_six(six);
-	// printf("\n");
 	char *six = bin2SixString(bin, bin_len);
 	printf("%s\n", six);
 }
