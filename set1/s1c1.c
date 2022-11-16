3ecacf45f5ddc54720cbd334bb83f7eaa9708fa6
adamstucci <adamstucci@gmail.com>
2022-11-16 12:52:21 +1100

    notes

--- set1/s1c1.c
+++ set1/s1c1.c
@@ -18,10 +18,10 @@
 #define BUF_SIZE 1024
 
 int main(int argc, char **argv) {
-	char buffer[BUF_SIZE];
-	assert(fgets(buffer, BUF_SIZE, stdin) != NULL);
+	char buffer[BUF_SIZE + 1]; //+1 for null terminator
+	assert(fgets(buffer, BUF_SIZE+1, stdin) != NULL);
 	int len = strlen(buffer);
-	if (buffer[len-1] == '\n') buffer[len-1] = '\0';
+	if (buffer[len-1] == '\n') buffer[len-1] = '\0'; //chomp
 	int bin_len = 0;
 	unsigned char *bin = hex2bin(buffer, &bin_len);
 	printf("%d\n", bin_len);
